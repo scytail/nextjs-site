fa62e8b6557a7697f25e05aa11fb6372af941396
Ben S <bspymaster@gmail.com>
2023-11-12 21:08:14 -0600

    fix linter issue

--- components/about.tsx
+++ components/about.tsx
@@ -9,7 +9,7 @@ export default function About() {
         My journey started back in 2011 with building custom programs on my TI-84 and building small python applications to help me with day-to-day tasks. Fast-forward through high school and graduating Saint Louis University with a degree in Computer Science, and my interest in software development has not waned.
         <br />
         <br />
-        My professional and personal career has led me to full-stack development on an enterprise tech-stack, experimenting with building my own file management API, building various unity and godot games, constructing and managing a discord management bot used on servers with hundreds of users, and even crafting the custom-built website you're looking at.
+        My professional and personal career has led me to full-stack development on an enterprise tech-stack, experimenting with building my own file management API, building various unity and godot games, constructing and managing a discord management bot used on servers with hundreds of users, and even crafting the custom-built website you&apos;re looking at.
         <br />
         <br />
         When not working on software development, I am often found writing stories set in fantastical lands, going on (mis)adventures with friends, and running from hordes of  blocky zombies.
