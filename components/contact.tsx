6d2a6005653f9305d05ab82dc98dfb6d13329f41
Ben S <bspymaster@gmail.com>
2023-11-12 21:39:48 -0600

    updated email form message

--- components/contact.tsx
+++ components/contact.tsx
@@ -3,7 +3,9 @@ export default function Contact() {
     <div>
       <h1 className="text-4xl text-white mb-2">Send me an email!</h1>
       <div className="text-white flex-wrap">
-        Email form here
+        An email form is coming soon.
+        <br />
+        In the meantime, please reach out to me via <a className="hover:underline decoration-dotted" href="https://www.linkedin.com/in/ben-schwabe/" target="_blank">my LinkedIn</a>. Please be sure to mention that you came here from my website. 
       </div>
     </div>
   )
