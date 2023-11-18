26b5f0759d44d3ba1c2c7d34e4b4cafd64af13d1
Ben S <bspymaster@gmail.com>
2023-11-17 19:58:11 -0600

    project border color change

--- components/projects.tsx
+++ components/projects.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
       <h1 className="text-4xl text-white mb-2">Personal Projects</h1>
       <div className="text-white flex-wrap">
         <div className="grid grid-cols-2 gap-5 my-5">
-          <div className="flex flex-col border-2 border-white rounded-xl p-2">
+          <div className="flex flex-col border-2 border-slate-500 rounded-xl p-2">
             <h2 className="text-2xl font-bold">Title</h2>
             <span className="flex-grow text-base">Description Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</span>
             <div className="flex flex-row flex-wrap gap-1 mt-2">
@@ -12,7 +12,7 @@ export default function Projects() {
               <div className="bg-sky-500 text-slate-800 px-3 rounded-full">SQL</div>
             </div>
           </div>
-          <div className="flex flex-col border-2 border-white rounded-xl p-2">
+          <div className="flex flex-col border-2 border-slate-500 rounded-xl p-2">
             <h2 className="text-2xl font-bold">Title</h2>
             <span className="flex-grow text-base">Description Lorem Ipsum</span>
             <div className="flex flex-row flex-wrap gap-1 mt-2">
