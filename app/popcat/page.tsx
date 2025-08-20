f4dddc0e475ee1b7d79c7ba360a27b1ead8be2cb
Ben Schwabe <bspymaster@gmail.com>
2025-08-20 14:44:39 -0500

    added popcat

---
+++ app/popcat/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import Image from "next/image";
+import React from "react";
+
+export default function PopcatPage() {
+  const [isPopcatOpen, setIsPopcatOpen] = React.useState(false);
+  function setPopcatOpen() {
+    setIsPopcatOpen(true);
+    const audio = new Audio('/pop-u_8e8ungop1x.mp3');
+    audio.play();
+  }
+  function setPopcatClosed() {
+    setIsPopcatOpen(false);
+  }
+  return (
+    <main
+      className="min-h-screen flex justify-center items-center bg-gray-900"
+      onMouseDown={setPopcatOpen}
+      onMouseUp={setPopcatClosed}
+      onTouchStart={setPopcatOpen}
+      onTouchEnd={setPopcatClosed}
+    >
+      <Image
+      src={isPopcatOpen ? "/popcat-open.jpg" : "/popcat-closed.jpg"}
+      alt={isPopcatOpen ? "Popcat Open" : "Popcat Closed"}
+      width={400}
+      height={400}
+      draggable={false}
+      style={{ userSelect: "none" }}
+      className="rounded-lg"
+      />
+    </main>
+  );
+}
